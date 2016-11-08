3aa9921f5acdd6a3e42bc230cc279a3a197c65e0
Lawrence Pan <lawrencefeipan@gmail.com>
2016-11-08 13:48:45 -0500

    add build option

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,29 @@ const BUILD_PATH = path.join(__dirname, 'public');
 
 const DashboardPlugin = require('webpack-dashboard/plugin');
 
+const ENV = process.env.NODE_ENV;
+
+const getPlugins = (environment) => {
+  if (environment === 'production') {
+    return [
+      new webpack.optimize.OccurrenceOrderPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          screw_ie8: true,
+          warnings: false,
+        },
+      }),
+    ];
+  }
+
+  return [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new DashboardPlugin(),
+  ];
+};
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
 
@@ -31,9 +54,5 @@ module.exports = {
     ],
   },
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new DashboardPlugin(),
-  ],
+  plugins: getPlugins(ENV),
 };
